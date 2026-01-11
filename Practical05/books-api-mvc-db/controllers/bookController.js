b953cf3fdf6bd43d16eedd1007803ca457ba02cc
Amos9024 <hoamos90@gmail.com>
2026-01-11 21:05:38 +0800

    In Progress practical05 PartA

---
+++ Practical05/books-api-mvc-db/controllers/bookController.js
@@ -0,0 +1,76 @@
+const bookModel = require("../models/bookModel");
+
+// Get all books
+async function getAllBooks(req, res) {
+  try {
+    const books = await bookModel.getAllBooks();
+    res.json(books);
+  } catch (error) {
+    console.error("Controller error:", error);
+    res.status(500).json({ error: "Error retrieving books" });
+  }
+}
+
+// Get book by ID
+async function getBookById(req, res) {
+  try {
+    const id = parseInt(req.params.id);
+    const book = await bookModel.getBookById(id);
+    if (!book) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    res.json(book);
+  } catch (error) {
+    console.error("Controller error:", error);
+    res.status(500).json({ error: "Error retrieving book" });
+  }
+}
+
+// Create new book
+async function createBook(req, res) {
+  try {
+    const newBook = await bookModel.createBook(req.body);
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.error("Controller error:", error);
+    res.status(500).json({ error: "Error creating book" });
+  }
+}
+
+// Update an existing book
+async function updateBook(req, res) {
+  try {
+    const id = parseInt(req.params.id);
+    const updatedBook = await bookModel.updateBook(id, req.body);
+    if (updatedBook === 0) {
+      return res.status(404).json({ message: `No book found with id ${id}.` });
+    }
+    res.status(200).json({ message: "Book updated successfully." });
+  } catch (error) {
+    console.error("Controller error:", error);
+    res.status(500).json({ error: "Error updating book" });
+  }
+}
+
+// Delete a book
+async function deleteBook(req, res) {
+  try {   
+    const id = parseInt(req.params.id);
+    const message = await bookModel.deleteBook(id);
+    if (message === 0) {
+      return res.status(404).json({ message: `No book found with id ${id}.` });
+    }
+    res.json({message: `Book with id ${id} deleted successfully.`});
+  } catch (error) {
+    console.error("Controller error:", error);
+    res.status(500).json({ error: "Error deleting book" });
+  }
+}
+module.exports = {
+  getAllBooks,
+  getBookById,
+  createBook,
+  updateBook,
+  deleteBook,
+};
