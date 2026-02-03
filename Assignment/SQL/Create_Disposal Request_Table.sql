428539693f50102a1c980f6042d1c728bbf4fa85
Amos9024 <hoamos90@gmail.com>
2026-02-03 19:28:16 +0800

    In Progress Assignment

---
+++ Assignment/SQL/Create_Disposal Request_Table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE DisposalRequest (
+	DisposalID	 INT IDENTITY(1001,1) NOT NULL,
+	BinID		 INT NOT NULL,
+	UserID		 INT NOT NULL, 
+	DateDisposed  DATE NOT NULL,
+	SerialNumber smallint NOT NULL,
+	ModelName    varchar(50) NOT NULL,
+	Brand        varchar(50) NULL,
+	Weight		 smallint NOT NULL,
+	CONSTRAINT   PK_DisposalRequest PRIMARY KEY (DisposalID),
+	CONSTRAINT   FK_DisposalRequest_Bin FOREIGN KEY (BinID) REFERENCES Bins(BinID),
+	CONSTRAINT   FK_DisposalRequest_User FOREIGN KEY (UserID) REFERENCES Users(UserID),
+);
---
+++ Assignment/SQL/Create_Sample Data.sql
@@ -0,0 +1,24 @@
+INSERT INTO Users (Name, EmailAddr, Contact, Status, DateJoined)
+VALUES
+('Alice Lee', 'alice.lee@email.com', '98745612', 'A', '2023-01-15'),
+('Bob Lim', 'bob.lim@email.com', NULL, 'A', '2023-03-10'),
+('Carol Ng', 'carol.ng@email.com', '89786541', 'A', '2022-11-05'),
+('John Tan', 'john.tan@email.com', NULL, 'A', '2023-04-09');
+
+
+
+INSERT INTO Bins (LocationID, CurrentCapacity, MaxCapacity, Country, BinStatus)
+VALUES
+('SG1', 30, 120, 'Singapore','A'),
+('JP1', 75, 120, 'Japan','A'),
+('HK1', 50, 100, 'HongKong', 'A'),
+('HK2', 30, 100, 'HongKong', 'A');
+
+
+
+INSERT INTO DisposalRequest 
+(BinID, UserID, DateDisposed, SerialNumber, ModelName, Brand, Weight)
+VALUES
+(101, 1, '2026-01-05', 5501, 'HP EG0300FBDSP', 'HP',1),
+(102, 2, '2026-01-12', 5502, 'HP MB1000GCEHH', 'HP',2),
+(103, 3, '2026-01-20', 5003, 'Dell ST1000NM0033', 'Dell',2);
